be16718430fd08a974ba87e1c1504d3ef07eac14
Emil Avara <emilavara@hotmail.com>
2024-11-04 00:53:48 +0100

    more various progress

---
+++ js/main.js
@@ -0,0 +1,53 @@
+const portfolio = [
+    {
+        name: 'Wewoosh',
+        date: '2021 — 2023',
+        description: 'Application • WYSIWYG-tool • CMS',
+        image: ''
+    },
+    {
+        name: 'Suniai Oliva',
+        date: '2022',
+        description: 'Website',
+        image: ''
+    },
+    {
+        name: 'Manaforge',
+        date: '2021 — 2023',
+        description: 'Application • Website',
+        image: ''
+    },
+    {
+        name: 'Createweb',
+        date: '2024 —',
+        description: 'Website',
+        image: ''
+    },
+    {
+        name: 'Hermitage',
+        date: '2024 —',
+        description: 'Website',
+        image: ''
+    }
+]
+
+function render() {
+
+}
+
+render();
+
+/*
+<div class="item-cell">
+    <div class="image"></div>
+    <div class="item-cell-info-container">
+        <div class="item-title-date-container">
+            <h1 class="item-cell-title">Wewoosh</h1>
+            <p class="item-cell-date">2021 &mdash; 2023</p>
+        </div>
+        <div class="item-subtitle-container">
+            <h2 class="item-cell-subtitle">Application &bullet; WYSIWYG-tool &bullet; CMS</h2>
+        </div>
+    </div>
+</div>
+*/
